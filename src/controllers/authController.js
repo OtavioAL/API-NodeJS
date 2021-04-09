69eff1acdf5976ec4a1facafc74292099e5af87b
OtavioAL <54159437+OtavioAL@users.noreply.github.com>
2021-04-09 08:55:57 -0300

    Segundo Commit

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const authConfig = require('../config/auth.json');
 
 const User = require('../models/user');
 
 const router = express.Router();
+
+function generationToken(params = {}){
+  return jwt.sign( params, authConfig.secret, {
+    expiresIn: 86400,
+  });
+}
 
 router.post('/register', async(req, res) => {
   try{
@@ -17,10 +26,38 @@ router.post('/register', async(req, res) => {
 
     user.password = undefined;
 
-    return res.send({ user });
+    return res.send({ 
+      user, 
+      token: generationToken({ id: user.id }),
+    });
   }catch(err){
     return res.status(400).send({ error: 'Registration Failed' });
   }
+});
+
+router.post('/authenticate', async(req, res) =>{
+  const { email, password } = req.body;
+
+  /*
+    Necessario o select('+.password') pois estava definido na tabela que
+    em uma buscas não era para informar o password com isso ira fazer
+    com que seja possivel obter o password na busca realizada
+  */
+  const user = await User.findOne({ email }).select('+password');
+
+  if(!user)
+    return res.status(400).send({ error: 'Use not found' });
+
+  if(!await bcrypt.compare(password, user.password))
+    return res.status(400).send({ error:'Invalid password' });
+
+  user.password = undefined;
+
+  res.send({ 
+    user, 
+    token: generationToken({ id: user.id }),
+  });
+
 });
 
 module.exports = app => app.use('/auth', router);
