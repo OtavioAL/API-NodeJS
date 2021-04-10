a15d84fae0e2725f81434b50b5a4d4a5a0d2ba64
OtavioAL <54159437+OtavioAL@users.noreply.github.com>
2021-04-10 19:03:41 -0300

    Projeto Finalizado

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -2,9 +2,95 @@ const express = require('express');
 const authMiddleware = require('../middlewares/auth');
 const router = express.Router();
 
+const Project = require('../models/project');
+const Task = require('../models/task');
+
+
 router.use(authMiddleware);
-router.get('/', (req, res) => {
-    res.send({ ok: true, user: req.userId });
+//List  
+router.get('/', async (req, res) => {
+  try{
+    const projects = await Project.find().populate(['user', 'task']);
+
+    return res.send({ projects });
+  }catch(err){
+    return res.status(400).send({ error: 'Error loading projects' });
+  }
+});
+//Show
+router.get('/:projectId', async(req, res) => {
+  try{
+    const project = await Project.findById(req.params.projectId).populate(['user', 'task']);
+
+    return res.send({ project });
+  }catch(err){
+    return res.status(400).send({ error: 'Error loading project' });
+  }
+});
+//Create
+router.post('/', async(req, res) =>{
+  try{
+    const { title, description, tasks } = req.body;   
+
+    const project = await Project.create({ title, description, user: req.userId });
+
+    await Promise.all(tasks.map(async task => {
+      const projectTask = new Task({ ...task, project: project._id });
+    
+      await projectTask.save();
+
+      project.tasks.push(projectTask);
+
+    }));
+
+    await project.save();
+
+    return res.send({ project });
+  }catch(err){
+    return res.status(400).send({ error: 'Error creating new project' });
+  }
+});
+//Update
+router.put('/:projectId', async(req, res) => {
+  try{
+    const { title, description, tasks } = req.body;   
+
+    const project = await Project.findByIdAndUpdate(req.params.projectId, 
+      { 
+        title, 
+        description,
+        //Responsavel por fazer com que retorne o projeto atualizado
+      }, { new: true });
+
+    //Deletando as tasks ja existentes antes de atualizar
+    project.tasks = [];
+    await Task.deleteOne({ project: project._id });
+
+    await Promise.all(tasks.map(async task => {
+      const projectTask = new Task({ ...task, project: project._id });
+    
+      await projectTask.save();
+
+      project.tasks.push(projectTask);
+
+    }));
+
+    await project.save();
+
+    return res.send({ project });
+  }catch(err){
+    return res.status(400).send({ error: 'Error update project' });
+  }
+});
+//Delete
+router.delete('/:projectId', async(req, res) => {
+  try{
+    await Project.findByIdAndRemove(req.params.projectId);
+
+    return res.send(); 
+  }catch(err){
+    return res.status(400).send({ error: 'Error deleting project' });
+  }
 });
 
 module.exports = app => app.use('/projects', router);
---
+++ src/app/models/task.js
@@ -0,0 +1,33 @@
+const mongoose = require('../../database/index');
+const bcrypt = require('bcryptjs');
+//Schema os campos que vamos ter dentro da tabela 
+const TaskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    require: true,
+  },
+  project:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Project',
+    require: true,
+  },
+  assignedTo:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    require: true,
+  },
+  completed:{
+    type: Boolean,
+    require: true,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+
+const Task = mongoose.model('Task', TaskSchema);
+
+module.exports = Task;
